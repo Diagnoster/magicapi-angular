5af3f6f2f989a7d175f02e56f1202183f2ba0e22
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-08-20 13:53:54 -0300

    feat: adding artist name parameter

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -9,13 +9,14 @@ export class Card {
   cmc: string;
   number: string;
   text: string;
+  artist: string;
   power: string;
   toughness: string;
   imageUrl: string;
   flavor: string;
   legalities: Legalities[];
 
-  constructor(name: string, type: string, rarity: string, set: string, setName: string, cmc: string, number: string, text: string, power: string, toughness: string, imageUrl: string, flavor: string, legalities: Legalities[]) {
+  constructor(name: string, type: string, rarity: string, set: string, setName: string, cmc: string, number: string, text: string, artist: string, power: string, toughness: string, imageUrl: string, flavor: string, legalities: Legalities[]) {
     this.name = name;
     this.type = type;
     this.rarity = rarity;
@@ -24,6 +25,7 @@ export class Card {
     this.cmc = cmc;
     this.number = number;
     this.text = text;
+    this.artist = artist;
     this.power = power;
     this.toughness = toughness;
     this.imageUrl = imageUrl;
