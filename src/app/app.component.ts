23e12b891e642b6d658341e77029cd6e5e9d417c
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-08-21 13:55:58 -0300

    feat: adding function for working insights of the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardDetailsComponent } from './components/card-details/card-details.component';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,10 @@ import { CardDetailsComponent } from './components/card-details/card-details.com
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mgt-api';
+
+  ngOnInit(): void {
+		injectSpeedInsights();
+	}
 }
